58407e92a34db6a9dfdcae418e3006442e205ac1
ksskol <skolotakristina2@gmail.com>
2024-05-20 20:03:03 +0100

    added endpoint films

---
+++ src/controllers/films-controller.ts
@@ -0,0 +1,14 @@
+import { Response, Request } from "express";
+import { Film } from "../models/film-model";
+import { dbClose, dbOpen } from "../db-connection";
+
+export const getFilms = async (req: Request, res: Response) => {
+  try {
+    await dbOpen();
+    const films = await Film.find();
+    res.status(200).send({ films });
+    await dbClose();
+  } catch (err) {
+    console.log("oh no, films error", err);
+  }
+};
