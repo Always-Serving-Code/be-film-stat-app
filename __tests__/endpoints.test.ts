e97e0d1147bf29e1757906eb01916f2a13c8f0b3
Pumpkin92 <77683675+Pumpkin92@users.noreply.github.com>
2024-05-21 09:18:40 +0100

    adds first users test

--- __tests__/endpoints.test.ts
+++ __tests__/endpoints.test.ts
@@ -1,14 +1,32 @@
-import app from '../src';
-import request from 'supertest';
+import app from "../src";
+import request from "supertest";
 
-describe('/api', () => {
-	test('GET /api - responds with endpoints.json', async () => {
-		const { body } = await request(app).get('/api').expect(200);
-		console.log(body.endpoints);
-		expect(body.endpoints).toMatchObject({
-			'GET /api': {
-				description: expect.any(String),
-			},
-		});
-	});
+describe("/api", () => {
+  test("GET /api - responds with endpoints.json", async () => {
+    const { body } = await request(app).get("/api").expect(200);
+    console.log(body.endpoints);
+    expect(body.endpoints).toMatchObject({
+      "GET /api": {
+        description: expect.any(String),
+      },
+    });
+  });
+});
+
+describe("/api/users", () => {
+  test("GET /api - responds with an array of all users", async () => {
+    const { body } = await request(app).get("/api/users").expect(200);
+    console.log(body);
+    const { users } = body;
+    expect(users.length).toBe(1);
+    users.forEach((user: object) => {
+      expect(user).toMatchObject({
+        username: String,
+        password: String,
+        email: String,
+        films: Array,
+        stats: Object,
+      });
+    });
+  });
 });
