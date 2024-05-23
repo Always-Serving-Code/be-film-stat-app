fd107139309acd66ce71179e3326fdf62b021865
Pumpkin92 <77683675+Pumpkin92@users.noreply.github.com>
2024-05-23 08:54:31 +0100

    small refactor

--- __tests__/endpoints.test.ts
+++ __tests__/endpoints.test.ts
@@ -4,17 +4,17 @@ import seed from "../src/data/seed";
 import { dbOpen } from "../src/db-connection";
 
 beforeAll(async () => {
-	await dbOpen();
+  await dbOpen();
 });
 beforeEach(async () => {
-	await seed();
+  await seed();
 });
 
-describe('404 General Not Found Error', () => {
-	test('404: When path does not exist', async () => {
-		const { body } = await request(app).get('/api/incorrect-path').expect(404);
-		expect(body.msg).toBe('Not Found');
-	});
+describe("404 General Not Found Error", () => {
+  test("404: When path does not exist", async () => {
+    const { body } = await request(app).get("/api/incorrect-path").expect(404);
+    expect(body.msg).toBe("Not Found");
+  });
 });
 
 describe("/api", () => {
@@ -28,22 +28,22 @@ describe("/api", () => {
   });
 });
 
-describe('/api/users', () => {
-	test('GET 200 /api/users  - responds with an array of all users', async () => {
-		const { body } = await request(app).get('/api/users').expect(200);
-		const { users } = body;
-		expect(users.length).toBe(5);
-		users.forEach((user: object) => {
-			expect(user).toMatchObject({
-				_id: expect.any(Number),
-				username: expect.any(String),
-				password: expect.any(String),
-				email: expect.any(String),
-				films: expect.any(Array),
-				stats: expect.any(Object),
-			});
-		});
-	});
+describe("/api/users", () => {
+  test("GET 200 /api/users  - responds with an array of all users", async () => {
+    const { body } = await request(app).get("/api/users").expect(200);
+    const { users } = body;
+    expect(users.length).toBe(5);
+    users.forEach((user: object) => {
+      expect(user).toMatchObject({
+        _id: expect.any(Number),
+        username: expect.any(String),
+        password: expect.any(String),
+        email: expect.any(String),
+        films: expect.any(Array),
+        stats: expect.any(Object),
+      });
+    });
+  });
 });
 
 describe("/api/users/:user_id", () => {
@@ -74,103 +74,103 @@ describe("/api/users/:user_id", () => {
   });
 });
 
-describe('/api/users/:userId/films', () => {
-	test('GET 200 /api/users/:user_id/films', async () => {
-		const { body } = await request(app).get('/api/users/5/films').expect(200);
-		const { films } = body;
-		expect(films.length).toBe(4);
-		films.forEach((film: object) => {
-			expect(film).toMatchObject({
-				_id: expect.any(Number),
-				title: expect.any(String),
-				directors: expect.any(Array),
-				genres: expect.any(Array),
-				release_year: expect.any(Number),
-				synopsis: expect.any(String),
-				poster_url: expect.any(String),
-				lead_actors: expect.any(Array),
-				runtime: expect.any(Number),
-				rating: expect.any(Number),
-				date_watched: expect.any(String),
-			});
-		});
-	});
-	test('GET 404 /api/users/:user_id/films - user id not found', async () => {
-		const { body } = await request(app)
-			.get('/api/users/6000/films')
-			.expect(404);
-		expect(body.msg).toBe('Not Found');
-	});
-	test('GET 404 /api/users/:user_id/films - user exists but no associated films', async () => {
-		const { body } = await request(app).get('/api/users/1/films').expect(404);
-		expect(body.msg).toBe('No Films Added Yet!');
-	});
-	test('GET 400 /api/users/:user_id/films - invalid user id', async () => {
-		const { body } = await request(app)
-			.get('/api/users/garbage/films')
-			.expect(400);
-		expect(body.msg).toBe('Bad Request');
-	});
+describe("/api/users/:userId/films", () => {
+  test("GET 200 /api/users/:user_id/films", async () => {
+    const { body } = await request(app).get("/api/users/5/films").expect(200);
+    const { films } = body;
+    expect(films.length).toBe(4);
+    films.forEach((film: object) => {
+      expect(film).toMatchObject({
+        _id: expect.any(Number),
+        title: expect.any(String),
+        directors: expect.any(Array),
+        genres: expect.any(Array),
+        release_year: expect.any(Number),
+        synopsis: expect.any(String),
+        poster_url: expect.any(String),
+        lead_actors: expect.any(Array),
+        runtime: expect.any(Number),
+        rating: expect.any(Number),
+        date_watched: expect.any(String),
+      });
+    });
+  });
+  test("GET 404 /api/users/:user_id/films - user id not found", async () => {
+    const { body } = await request(app)
+      .get("/api/users/6000/films")
+      .expect(404);
+    expect(body.msg).toBe("Not Found");
+  });
+  test("GET 404 /api/users/:user_id/films - user exists but no associated films", async () => {
+    const { body } = await request(app).get("/api/users/1/films").expect(404);
+    expect(body.msg).toBe("No Films Added Yet!");
+  });
+  test("GET 400 /api/users/:user_id/films - invalid user id", async () => {
+    const { body } = await request(app)
+      .get("/api/users/garbage/films")
+      .expect(400);
+    expect(body.msg).toBe("Bad Request");
+  });
 });
 
-describe('/api/films', () => {
-	test('GET /api/films - responds with an array of all films', async () => {
-		const { body } = await request(app).get('/api/films').expect(200);
-		const { films } = body;
-		expect(films.length).toBe(30);
-		films.forEach((film: object) => {
-			expect(film).toMatchObject({
-				_id: expect.any(Number),
-				title: expect.any(String),
-				directors: expect.any(Array),
-				genres: expect.any(Array),
-				release_year: expect.any(Number),
-				synopsis: expect.any(String),
-				poster_url: expect.any(String),
-				lead_actors: expect.any(Array),
-				runtime: expect.any(Number),
-			});
-		});
-	});
+describe("/api/films", () => {
+  test("GET /api/films - responds with an array of all films", async () => {
+    const { body } = await request(app).get("/api/films").expect(200);
+    const { films } = body;
+    expect(films.length).toBe(30);
+    films.forEach((film: object) => {
+      expect(film).toMatchObject({
+        _id: expect.any(Number),
+        title: expect.any(String),
+        directors: expect.any(Array),
+        genres: expect.any(Array),
+        release_year: expect.any(Number),
+        synopsis: expect.any(String),
+        poster_url: expect.any(String),
+        lead_actors: expect.any(Array),
+        runtime: expect.any(Number),
+      });
+    });
+  });
 });
 
-describe('/api/users/:user_id/:film_id', () => {
-	test('DELETE 204 /api/users/:user_id/:film_id - removes an existing film from the users history', async () => {
-		await request(app).delete('/api/users/5/1').expect(204);
-	});
-	test('DELETE 204 /api/users/:user_id/:film_id - stats associated with the user are updated', async () => {
-		await request(app).delete('/api/users/5/1').expect(204);
-		const { body } = await request(app).get('/api/users/5').expect(200);
-		expect(body.user[0].stats).toMatchObject({
-			num_films_watched: 3,
-			hours_watched: 584,
-		});
-	});
-	test('DELETE 404 /api/users/:user_id/:film_id - non-existent user id', async () => {
-		const { body } = await request(app)
-			.delete('/api/users/50000/1')
-			.expect(404);
-		expect(body.msg).toBe('Not Found');
-	});
-	test('DELETE 404 /api/users/:user_id/:film_id - non-existent film id', async () => {
-		const { body } = await request(app)
-			.delete('/api/users/5/50000')
-			.expect(404);
-		expect(body.msg).toBe('Not Found');
-	});
-	test('DELETE 400 /api/users/:user_id/:film_id - invalid user id', async () => {
-		const { body } = await request(app)
-			.delete('/api/users/garbage/1')
-			.expect(400);
-		expect(body.msg).toBe('Bad Request');
-	});
-	test('DELETE 400 /api/users/:user_id/:film_id - invalid film id', async () => {
-		const { body } = await request(app)
-			.delete('/api/users/5/garbage')
-			.expect(400);
-		expect(body.msg).toBe('Bad Request');
-	});
-
+describe("/api/users/:user_id/:film_id", () => {
+  test("DELETE 204 /api/users/:user_id/:film_id - removes an existing film from the users history", async () => {
+    await request(app).delete("/api/users/5/1").expect(204);
+  });
+  test("DELETE 204 /api/users/:user_id/:film_id - stats associated with the user are updated", async () => {
+    await request(app).delete("/api/users/5/1").expect(204);
+    const { body } = await request(app).get("/api/users/5").expect(200);
+    expect(body.user[0].stats).toMatchObject({
+      num_films_watched: 3,
+      hours_watched: 584,
+    });
+  });
+  test("DELETE 404 /api/users/:user_id/:film_id - non-existent user id", async () => {
+    const { body } = await request(app)
+      .delete("/api/users/50000/1")
+      .expect(404);
+    expect(body.msg).toBe("Not Found");
+  });
+  test("DELETE 404 /api/users/:user_id/:film_id - non-existent film id", async () => {
+    const { body } = await request(app)
+      .delete("/api/users/5/50000")
+      .expect(404);
+    expect(body.msg).toBe("Not Found");
+  });
+  test("DELETE 400 /api/users/:user_id/:film_id - invalid user id", async () => {
+    const { body } = await request(app)
+      .delete("/api/users/garbage/1")
+      .expect(400);
+    expect(body.msg).toBe("Bad Request");
+  });
+  test("DELETE 400 /api/users/:user_id/:film_id - invalid film id", async () => {
+    const { body } = await request(app)
+      .delete("/api/users/5/garbage")
+      .expect(400);
+    expect(body.msg).toBe("Bad Request");
+  });
+});
 describe("/api/users/:user_id", () => {
   test("PATCH 200 /api/users/:user_id - responds with an object with an updated user after adding a film", async () => {
     const film = {
@@ -227,6 +227,8 @@ describe("/api/users/:user_id", () => {
       .send({ films: film })
       .expect(200);
     const { user } = body;
+    console.log(user);
+    console.log(typeof user.films[0].date_watched);
 
     expect(user).toMatchObject({
       _id: 5,
@@ -235,9 +237,9 @@ describe("/api/users/:user_id", () => {
       email: "pumpkin@gmail.com",
       films: [
         {
-          _id: "1",
+          _id: 1,
           title: "The Lord of The Rings: The Fellowship of the Ring",
-          directors: "Peter Jackson",
+          directors: ["Peter Jackson"],
           genres: ["fantasy", "action", "adventure"],
           release_year: 2001,
           synopsis:
@@ -246,12 +248,12 @@ describe("/api/users/:user_id", () => {
           lead_actors: ["Elijah Wood", "Ian McKellen", "Viggo Mortensen"],
           runtime: 178,
           rating: 5,
-          date_watched: expect.any(String),
+          date_watched: expect.any("string"),
         },
         {
-          _id: "2",
+          _id: 2,
           title: "The Lord of The Rings: The Two Towers",
-          directors: "Peter Jackson",
+          directors: ["Peter Jackson"],
           genres: ["fantasy", "action", "adventure"],
           release_year: 2002,
           synopsis:
@@ -261,12 +263,12 @@ describe("/api/users/:user_id", () => {
           lead_actors: ["Elijah Wood", "Ian McKellen", "Viggo Mortensen"],
           runtime: 235,
           rating: 5,
-          date_watched: expect.any(String),
+          date_watched: expect.any("string"),
         },
         {
-          _id: "3",
+          _id: 3,
           title: "The Lord of The Rings: The Return of the King",
-          directors: "Peter Jackson",
+          directors: ["Peter Jackson"],
           genres: ["fantasy", "action", "adventure"],
           release_year: 2003,
           synopsis:
@@ -276,12 +278,12 @@ describe("/api/users/:user_id", () => {
           lead_actors: ["Elijah Wood", "Ian McKellen", "Viggo Mortensen"],
           runtime: 201,
           rating: 5,
-          date_watched: expect.any(String),
+          date_watched: expect.any("string"),
         },
         {
-          _id: "4",
+          _id: 4,
           title: "Midsommar",
-          directors: "Ari Aster",
+          directors: ["Ari Aster"],
           genres: ["drama", "horror", "mystery"],
           release_year: 2019,
           synopsis:
@@ -291,7 +293,7 @@ describe("/api/users/:user_id", () => {
           lead_actors: ["Florence Pugh", "Jack Reynor"],
           runtime: 148,
           rating: 4,
-          date_watched: expect.any(String),
+          date_watched: expect.any("string"),
         },
         film,
       ],
